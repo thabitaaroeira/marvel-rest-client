b54f3e29c8fdfc25d2d3e41c8eb6d35f1226d4e1
Thabita Aroeira <thabitaaroeira@gmail.com>
2016-11-12 19:05:51 -0200

    Initial commit

---
+++ marvel-rest-client/src/main/java/br/com/thabita/MarvelRestClientApplication.java
@@ -0,0 +1,12 @@
+package br.com.thabita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarvelRestClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarvelRestClientApplication.class, args);
+	}
+}
